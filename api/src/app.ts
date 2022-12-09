9da08d670d3c0970b2971fdefe53c8d0b9fe720d
Edgar Avecilla <edgaravecilla1993@gmail.com>
2022-12-08 18:58:45 -0600

    Added record validation

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,9 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import routes from './routes';
 import middleware from './middleware';
-import { Error } from './types';
-import { Genre, Videogame } from './db';
 
 export const server = express();
 
@@ -21,19 +19,6 @@ server.use(middleware.headersConfig);
 
 //$routes
 server.use('/', routes);
-server.get('/', async (req, res) => {
-  await Genre.create({
-    id: 1,
-    name: 'ejemplo',
-  });
-  res.send('se creo el genero');
-});
 
 // Error catching endware.
-server.use((err: Error, req: Request, res: Response, next: NextFunction): void => {
-  // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
+server.use(middleware.ErrorHandling);
